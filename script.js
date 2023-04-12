4454cf5615b55f83285611a9663311e76a17a557
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-04-13 01:38:12 +0400

    Single quote

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const hamburgerIcon = document.querySelector(".hamburgerIcon");
-const menulist = document.querySelector(".menu-list-items > ul");
-const menuListItems = document.querySelectorAll(".menu-list-items li");
+const hamburgerIcon = document.querySelector('.hamburgerIcon');
+const menulist = document.querySelector('.menu-list-items > ul');
+const menuListItems = document.querySelectorAll('.menu-list-items li');
 
-hamburgerIcon.addEventListener("click", () => {
-  menulist.classList.toggle("active");
+hamburgerIcon.addEventListener('click', () => {
+  menulist.classList.toggle('active');
 });
 
 menuListItems.forEach((el) => {
-  el.addEventListener("click", () => {
-    menulist.classList.toggle("active");
+  el.addEventListener('click', () => {
+    menulist.classList.toggle('active');
   });
 });
