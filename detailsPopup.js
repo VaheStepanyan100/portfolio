f34cd0cf383e9af3823f1a05a027c5ccd8fa9f25
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-04-19 21:34:20 +0400

    follow the background color of the space around the popup window

--- detailsPopup.js
+++ detailsPopup.js
@@ -108,10 +108,17 @@ const closeIcon = document.querySelector('.close-button');
 closeIcon.addEventListener('click', () => {
   detailPopup.classList.add('hide');
   const destruction = document.querySelectorAll(
-    '.info li, .skills li, .detail-image',
+    '.info li, .skills li, .detail-image'
   );
   for (let i = 0; i < destruction.length; i += 1) {
     destruction[i].parentNode.removeChild(destruction[i]);
+  }
+  if (detailPopup.classList.contains('hide')) {
+    document.querySelector('.header').style.backgroundColor = '';
+    document.querySelector('.intro').style.backgroundColor = '';
+    document.querySelector('.about-article').style.backgroundColor = '';
+    document.querySelector('.about-article').style.backgroundColor = '';
+    document.body.style.backgroundColor = '';
   }
 });
 
@@ -159,5 +166,14 @@ seeProject.forEach((el) => {
     detailPopup.prepend(detailTitle);
 
     detailPopup.classList.remove('hide');
+    if (!detailPopup.classList.contains('hide')) {
+      document.querySelector('.header').style.backgroundColor = '#C1C7D0';
+      document.querySelector('.intro').style.backgroundColor = '#C1C7D0';
+      document.querySelector('.about-article').style.backgroundColor =
+        '#C1C7D0';
+      document.querySelector('.about-article').style.backgroundColor =
+        '#C1C7D0';
+      document.body.style.backgroundColor = '#C1C7D0';
+    }
   });
 });
